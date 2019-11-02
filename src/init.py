68c379c8ee6c6d4a6c8b3950bd0c12baa7a52bdf
divyanshu25 <divyanshu.g25@gmail.com>
2019-11-02 22:56:14 +0000

    createing labels and adding progress bar

--- src/init.py
+++ src/init.py
@@ -4,6 +4,7 @@ from utils.data_directory_manager import DataDirectoryManager
 from landsatUtil.landsat.downloader import Downloader
 from utils.image_correction import LandsatTOACorrecter
 from utils.raster_tools import *
+from utils.rasterize import rasterize_label, save_raster
 from models.antarctic_rock_outcrop_os import OutcropLabeler
 from os import listdir
 from os.path import isfile, join
@@ -11,6 +12,8 @@ from _thread import *
 import threading 
 import matplotlib.pyplot as plt
 import rasterio.plot as rplt
+from progress.bar import Bar
+ 
 
 
 import numpy as np
@@ -35,9 +38,11 @@ def untar_helper(threadName, scene_IDs, chunkNum, dm, totalThreads):
         end_chunk = lenList
     else:
         end_chunk = (chunkNum+1)*chunkSize
-    scene_chunk = scene_IDs[start_chunk : end_chunk ]
+            
+    scene_chunk = scene_IDs[start_chunk : end_chunk]
     for s in scene_chunk:
         if s in un_compressed_data:
+            print(str(s) + " is already un-tarred")
             continue
         print("Thread: " + threadName + " Untarring: " + s)
         try:
@@ -54,10 +59,11 @@ if __name__ == "__main__":
     dm = DataDirectoryManager(os.path.join(base_dir,"data"))
     '''
     dm.download_supplement() # where we get the zip file
-    dm.extract_scene_id_file() # where we get the sceneID .txt
     '''
+    dm.extract_supplement_files() # where we get the sceneID .txt and outcrop .shp
+    
     scene_IDs = []
-    scene_IDs = [i["ID"] for i in dm.load_scene_ids()[:39]]
+    scene_IDs = [i["ID"] for i in dm.load_scene_ids()[:31]]
    
     dataFiles = [f for f in listdir(dataPath) if isfile(join(dataPath, f))]
     fName = [i.split(".")[0].replace("'", "") for i in dataFiles] 
@@ -65,11 +71,15 @@ if __name__ == "__main__":
         if s not in fName:
             scene_IDs.remove(s)
 
+    print("Downloaded SceneIDs: ")
+    print(scene_IDs)
     #Load Already Compressed Files
     with open(rawPath+ '/raw_file.txt', 'r') as filehandle:
         for line in filehandle:
             item = line[:-1]
             un_compressed_data.append(item)
+    print("Already Untarred Files: ")
+    print(un_compressed_data)
     try:
         t1 = threading.Thread( target = untar_helper, args = ("Thread-1", scene_IDs, 0, dm, 4, ) )
         t2 = threading.Thread( target = untar_helper, args = ("Thread-2", scene_IDs, 1, dm, 4, ) )
@@ -94,40 +104,93 @@ if __name__ == "__main__":
     rawFiles = [f for f in listdir(rawPath)]
     rawFName = [i.split(".")[0].replace("'", "") for i in rawFiles]
     
-    
-    
+         
+    # Create Chunks from Raw Images #
     bands = ["B1", "B2", "B3", "B4", "B5", "B6", "B7", "B9", "B10", "B11"]
-    for r in rawFName:
+    rasters = []
+    
+    if not os.path.exists(stackedPath):
+        os.mkdir(stackedPath)
+        
+    for r in un_compressed_data:
+        if os.path.exists(os.path.join(stackedPath, r)):
+            print("Scene {} already processed".format(r))
+            continue
+        # Generating the label of raw_file r
+        print("Processing image {}".format(r))
+        
+        band_2_path = os.path.join(dm.raw_image_dir, r, r + "_B2.TIF")
+        label_path = os.path.join(dm.label_dir, r + "_label.TIF")
+        if not os.path.exists(label_path):
+            try:
+                label, meta = rasterize_label(band_2_path, dm.outcrop_shape_path)
+                
+                meta['dtype'] = label.dtype
+                meta['count'] = 1
+                with rio.open(label_path, 'w', **meta) as dst:
+                    dst.write(label)
+                    
+            except ValueError as e:
+                with rio.open(band_2_path) as rockless:
+                    meta = rockless.meta.copy()
+                    zero_shape = (meta["width"], meta["height"])
+                label = np.zeros(zero_shape, np.int8)
+                print("label for image {} has no overlapping shapes".format(band_2_path))
+                
+                meta['dtype'] = label.dtype
+                meta['count'] = 1
+                with rio.open(label_path, 'w', **meta) as dst:
+                    dst.write(label, 1)
+        
+        with rio.open(label_path) as label_file:
+            label = label_file.read(1)
+        # label = plt.imread(label_path)
+        print(label.shape)
+        
+        # Label Created
+        
+        
         cnt = 0;
+        rasters.clear()
         for b in bands:
             imgName = "data/raw/"+r + "/" + r + "_"+ b + ".TIF"
             raster = plt.imread(imgName)
-            print(cnt, raster.shape)
+#             print(cnt, raster.shape)
             cnt = cnt+1
             rasters.append(raster)
 
         stacked_rasters = np.stack(rasters, axis=0).transpose(1,2,0)
-#         print(stacked_rasters.shape)
-        staked_chunks = [] 
-        imgSize = stacked_rasters.shape[0]
+        print("Shape of Stacked Images", stacked_rasters.shape)
+        stacked_chunks = [] 
+        imgSize = min(stacked_rasters.shape[0], stacked_rasters.shape[1])
         chunkSize = 512
         numChunks  = int(imgSize / chunkSize)
-#         print(numChunks)
         cnt = 0
-        dirName = stackedPath+"/"+rawFName[0] 
+        dirName = stackedPath+"/" + r
+        print("Creating Directory: " , dirName)
         if not os.path.exists(dirName):
+           
             os.mkdir(dirName)
-        for i in range(0,numChunks):
-            stIndex = i*chunkSize
-            endIndex = (i+1)*chunkSize 
-            chunk = stacked_rasters[stIndex:endIndex, stIndex:endIndex, :]   
-            fil = np.where(chunk>0)
-            nZP = np.sum(fil)
-            fileName = dirName + "/chunk_" + str(i) + ".npy" 
-            if(nZP > 0):
-                np.save(fileName, chunk, allow_pickle = True)
-                print(cnt, chunk.shape)
-            cnt = cnt +1
+        with Bar('Processing', max=numChunks*numChunks) as bar:
+            for i in range(0,numChunks):
+                for j in range(0, numChunks):
+#                     print("Chunk ({},{})".format(i, j))
+                    hstIndex = i*chunkSize
+                    hendIndex = (i+1)*chunkSize
+                    vstIndex = j*chunkSize
+                    vendIndex = (j+1)*chunkSize
+                    chunk = stacked_rasters[hstIndex:hendIndex, vstIndex:vendIndex, :]  
+                    label_chunk = label[hstIndex:hendIndex, vstIndex:vendIndex] 
+                    fil = np.where(chunk>0)
+                    nZP = np.sum(fil)
+                    fileName = os.path.join(dirName, "chunk_{}_{}.npy".format(i,j))
+                    labelFileName = os.path.join(dirName, "chunk_{}_{}_label.npy".format(i,j))
+                    if(nZP > 0):
+                        np.save(fileName, chunk, allow_pickle = True)
+                        np.save(labelFileName, label_chunk, allow_pickle = True)
+        #                 print(cnt, chunk.shape)
+                    cnt = cnt +1
+                    bar.next()
             
          
 
